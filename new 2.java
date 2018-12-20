b3eb3a5bec8a55d6ada56a457ce2c8cb05adc9b9
padhu <padmadadi99@gmail.com>
2018-12-20 04:48:45 -0800

    hi

---
+++ new 2.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public static void main(string args[])
+{
+int l=20;
+for(int i=0;i>=l;i++)
+{
+int result=i*i;
+system.out.println("the square of given number"+result);
+}
+}
